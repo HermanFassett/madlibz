94daa595f384e560758b46a161e926f6df95ad52
Herman Fassett <hermanfassett@gmail.com>
2016-01-29 00:36:01 +0000

    Initial commit

---
+++ bin/app.py
@@ -0,0 +1,63 @@
+# import modules
+import web
+import json
+import requests
+import random
+import SimpleHTTPServer
+
+# Set urls
+urls = (
+	'/', 'index',
+	'/api', 'api',
+	'/api/(.*)', 'madlibCall'
+)
+
+# Set app and render
+app = web.application(urls, globals())
+render = web.template.render('templates/', base="layout")
+
+# / Route
+class index(object):
+	def GET(self):
+		return render.index()
+		
+# /api route
+class api(object):
+	def GET(self):
+		return render.api()
+		
+# /api/random route
+class madlibCall(object):
+	def GET(self, type):
+		if (type == "random"):
+			# Set headers
+			web.header("Access-Control-Allow-Origin", "*") # Allow all access
+			web.header("Content-Type", "application/json") # Set content type
+			# Load json data
+			with open('data/templates.json') as data_file:
+				data = json.load(data_file)
+			# Return the random template
+			i = web.input(maxlength="100", minlength="0")
+			maxlength = 100
+			minlength = 0
+			# Convert 
+			if i["maxlength"].isdigit():
+				maxlength = int(i["maxlength"])
+			if i["minlength"].isdigit():
+				minlength = int(i["minlength"])
+			if maxlength < minlength: # Can't have min greater than max!
+				maxlength, minlength = minlength, maxlength # Swap variables
+			print minlength, maxlength
+			# Filter the data based on length
+			new_data = [elem for elem in data["templates"] if (len(elem["blanks"]) <= maxlength and len(elem["blanks"]) >= minlength)]
+			# Check if elements exist
+			if len(new_data) < 1:
+				return json.dumps({"error": "Invalid length range - No templates exist"})
+			# Get a random index for the templates
+			randomIndex = random.randint(0, len(new_data) - 1)
+			return json.dumps(new_data[randomIndex], indent=4)	# Send data
+		else:
+			return json.dumps({"error": "Invalid API call"})
+       
+if __name__ == "__main__":
+	app.run()
